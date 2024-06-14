4cfddbef26ed449ba606bceb24e0114d24812d2c
Jabier Zurro Aduriz <Jabier Zurro Aduriz@DESKTOP-F2EMQBQ>
2024-06-14 16:37:16 +0200

    Version 0. Creación de clases para la aplicación.

---
+++ src/main/java/proyectopersonal/buscadorprecios/AppBuscadorPecios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyectopersonal.buscadorprecios;
+
+/**
+ *
+ * @author Jabier Zurro Aduriz
+ */
+public class AppBuscadorPecios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/proyectopersonal/buscadorprecios/Supermercado.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectopersonal.buscadorprecios;
+
+/**
+ *
+ * @author Jabier Zurro Aduriz
+ */
+public class Supermercado {
+    
+}
