341f04c9a5f3f5316c949f26aab6e30aa24002e4
Jabier Zurro Aduriz <jabier_zurro@hotmail.com>
2024-06-14 16:43:20 +0200

    Version 0.1 Creación de clases para la aplicación y modificación del autor y commiter del proyecto.

--- src/main/java/proyectopersonal/buscadorprecios/Producto.java
+++ src/main/java/proyectopersonal/buscadorprecios/Producto.java
@@ -9,5 +9,13 @@ package proyectopersonal.buscadorprecios;
  * @author Jabier Zurro Aduriz
  */
 public class Producto {
-    
+        private String nombre;
+        private int cantidad;
+        private double precio;
+        
+public Producto(String nombre, int cantidad, double precio) {
+    this.nombre = nombre;
+    this.cantidad = cantidad;
+    this.precio = precio;
+}
 }
